4f643ed8acaa368cbb702195b29e468ddb4893eb
Jeki <142556335+Glenrejeki@users.noreply.github.com>
2024-11-11 13:59:41 +0700

    First push

---
+++ CampusFit/src/campusfit/CampusFit.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package campusfit;
+
+/**
+ *
+ * @author Glen Rejeki Sitorus
+ */
+public class CampusFit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World ! ");
+    }
+    
+}
